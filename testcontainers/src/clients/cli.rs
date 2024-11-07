d43ff943b7815cd212ded79db67bedbd1fd7494c
Cory Snyder <csnyder@1111systems.com>
2024-11-07 06:49:26 -0500

    output reason container failed

--- testcontainers/src/clients/cli.rs
+++ testcontainers/src/clients/cli.rs
@@ -45,7 +45,7 @@ impl Cli {
 
         let output = command.output().expect("Failed to execute docker command");
 
-        assert!(output.status.success(), "failed to start container");
+        assert!(output.status.success(), "failed to start container: {}", String::from_utf8_lossy(&output.stderr));
         let container_id = String::from_utf8(output.stdout)
             .expect("output is not valid utf8")
             .trim()
